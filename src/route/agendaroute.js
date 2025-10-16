7e855722b1545c86badda1a65cdd14dc020e5f5f
Matheo <matheo.vigneres@gmail.com>
2025-10-16 13:02:21 +0200

    Utilisation middleware

--- src/route/agendaroute.js
+++ src/route/agendaroute.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const isAuthenticated = require("../middleware/auth");
 
 // Route GET /agenda avec vérification de connexion
-router.get("/agenda", (req, res, next) => {
-  if (!req.session.userId) {
-    return res.status(401).json({ message: "Non connecté" });
-  }
+router.get("/agenda", isAuthenticated, (req, res) => {
   res.json({ message: "Bienvenue sur l'agenda !" });
 });
 
