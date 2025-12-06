2cc15fd9584ac02b2bc9f7ed1d23d5941f64c16f
kaba Sakho <kaba.sakho@edu.univ-paris13.fr>
2025-12-06 17:12:00 +0100

    Travail en cours (import/export)

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const path = require("path"); // Pour gérer les chemins de fichiers
+
+// Routes
 const authRoutes = require("./route/authroute");
 const testRoutes = require("./route/testroute");
 const agendaRoutes = require("./route/agendaroute");
-const session = require("express-session");
-const path = require("path"); // Pour gérer les chemins de fichiers
+const agendaExportRoutes = require("./src/route/agenda.export");
+const agendaImportRoutes = require("./src/route/agenda.import");
 
 const app = express();
 const PORT = 3000;
 
+// Middlewares globaux
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "../public"))); //par rapport a package.json
+app.use(express.static(path.join(__dirname, "../public"))); 
 
-mongoose
-  .connect("mongodb://localhost:27017/agendaApp")
-  .then(() => console.log("Connecté à MongoDB"))
-  .catch((err) => console.error(" Erreur MongoDB :", err));
-
-//Gestion de la connection avec session + cookie
+// Sessions AVANT les routes
 app.use(
   session({
     secret: "ton_secret", // à personnaliser
@@ -30,10 +30,20 @@ app.use(
   })
 );
 
+// Routes API
 app.use("/api/auth", authRoutes);
 app.use("/api/agenda", agendaRoutes);
+app.use("/api/agenda", agendaExportRoutes);
+app.use("/api/agenda", agendaImportRoutes);
 app.use("/api/test", testRoutes);
 
+// Connexion à MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/agendaApp")
+  .then(() => console.log("Connecté à MongoDB"))
+  .catch((err) => console.error(" Erreur MongoDB :", err));
+
+  // Démarrage du serveur
 app.listen(PORT, () =>
-  console.log(`🚀 Serveur en ligne : http://localhost:${PORT}`)
+  console.log(` Serveur en ligne : http://localhost:${PORT}`)
 );
